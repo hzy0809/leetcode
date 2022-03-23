31f9d364bbef0645886fb8899bf9a0fcaf57241f
hzy0809 <386625912@qq.com>
2022-03-23 23:37:45 +0800

    '300'

--- leetcode/300_最长递增子序列.py
+++ leetcode/300_最长递增子序列.py
@@ -36,9 +36,59 @@
 
 链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
 """
+import bisect
 from typing import List
 
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        pass
+        # 转移方程
+        # dp[i]=max(dp[j])+1,其中0≤j<i且num[j]<num[i]
+        n = len(nums)
+        dp = [1] * n
+        for i in range(1, n):
+            for j in range(0, i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+
+class CupiditySolution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [nums[0]]
+        for i in range(1, len(nums)):
+            if nums[i] > dp[-1]:
+                dp.append(nums[i])
+            elif nums[i] < dp[-1]:
+                index = bisect.bisect_left(dp, nums[i])
+                dp[index] = nums[i]
+        return len(dp)
+
+
+class SCupiditySolution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [nums[0]]
+        for i in range(1, len(nums)):
+            if nums[i] > dp[-1]:
+                dp.append(nums[i])
+            elif nums[i] < dp[-1]:
+                index = self.search(dp, nums[i])
+                dp[index] = nums[i]
+        return len(dp)
+
+    @staticmethod
+    def search(dp, x):
+        left = 0
+        right = len(dp)
+        while left < right:
+            mid = (left + right) // 2
+            if dp[mid] < x:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+
+
+if __name__ == '__main__':
+    print(SCupiditySolution().lengthOfLIS([3, 5, 6, 2, 5, 4, 19, 5, 6, 7, 12]))
+    # print(SCupiditySolution().search([2, 5, 6], 4))
