ef8af1030c185114fb9eb176ff42d0972bb6b54a
hzy <huzhenyu@aidigger.com>
2022-03-25 14:44:20 +0800

    10

--- leetcode/10_正则表达式匹配.py
+++ leetcode/10_正则表达式匹配.py
@@ -76,6 +76,32 @@ class DynamicSolution:
         return f[m][n]
 
 
+class SDynamicSolution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
+
+        def matches(a, b):
+            if a == 0:
+                return False
+            elif p[b - 1] == '.':
+                return True
+            else:
+                return s[a - 1] == p[b - 1]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 2]
+                    if matches(i, j - 1):
+                        dp[i][j] |= dp[i - 1][j]
+                else:
+                    if matches(i, j):
+                        dp[i][j] = dp[i - 1][j - 1]
+        return dp[m][n]
+
+
 if __name__ == '__main__':
     "mississippi"
     "mis*is*ip*."
